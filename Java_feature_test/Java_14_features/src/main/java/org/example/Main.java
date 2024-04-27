2531747a1f16fce809e463e7c63d55ef4e434db9
Amar Gurung <agurung@Amars-MBP.lan>
2024-04-27 19:35:09 -0400

    Java 14 | Example of Record

---
+++ Java_feature_test/Java_14_features/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.example.record.Employee;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Employee e = new Employee("Amar","Gurung","sss",1,"es");
+        System.out.println(e.departmentId());
+        System.out.println(e.departmentName());
+        e.
+    }
+}
---
+++ Java_feature_test/Java_14_features/src/main/java/org/example/record/Employee.java
@@ -0,0 +1,4 @@
+package org.example.record;
+
+public record Employee(String firstName,String lastName, String ssn,int departmentId,String departmentName) {
+}
